11f52beb9f3a5342cc7117313766b1ab4558a7e6
Majestic-Pako <agustinchoque484@gmail.com>
2024-11-15 12:47:22 -0300

    Update Menu.php
    Limpieza de las anteriores variables antes de pasarlo a la base de datos

--- Menu.php
+++ Menu.php
@@ -1,100 +1,10 @@
 <?php
-
-/*
-$productos = [
-    'cafe' => [
-        [
-            'nombre' => 'Café con leche',
-            'imagen' => 'img/Leche-f.png',
-            'categoria' => 'Café'
-        ],
-        [
-            'nombre' => 'Espresso',
-            'imagen' => '#',
-            'categoria' => 'Café'
-        ],
-        [
-            'nombre' => 'Latte',
-            'imagen' => '#',
-            'categoria' => 'Café'
-        ],
-        [
-            'nombre' => 'Americano',
-            'imagen' => '#',
-            'categoria' => 'Café'
-        ],
-        [
-            'nombre' => 'Capuccino',
-            'imagen' => '#',
-            'categoria' => 'Café'
-        ]
-    ],
-    'comida' => [
-        [
-            'nombre' => 'Tostadas',
-            'imagen' => 'img/Tostada-f.png',
-            'categoria' => 'Comida'
-        ],
-        [
-            'nombre' => 'Medialunas',
-            'imagen' => '#',
-            'categoria' => 'Comida'
-        ],
-        [
-            'nombre' => 'Crossaint',
-            'imagen' => '#',
-            'categoria' => 'Comida'
-        ],
-        [
-            'nombre' => 'Bagel Sandwich',
-            'imagen' => '#',
-            'categoria' => 'Comida'
-        ]
-    ],
-    'bebidas' => [
-        [
-            'nombre' => 'Te',
-            'imagen' => '#',
-            'categoria' => 'Bebidas'
-        ],
-        [
-            'nombre' => 'Frappuccino',
-            'imagen' => '#',
-            'categoria' => 'Bebidas'
-        ],
-        [
-            'nombre' => 'Te Helado',
-            'imagen' => '#',
-            'categoria' => 'Bebidas'
-        ],
-        [
-            'nombre' => 'Frutilla Acai',
-            'imagen' => '#',
-            'categoria' => 'Bebidas'
-        ],
-        [
-            'nombre' => 'Chocolate Caliente',
-            'imagen' => '#',
-            'categoria' => 'Bebidas'
-        ],
-        [
-            'nombre' => 'Submarino',
-            'imagen' => '#',
-            'categoria' => 'Bebidas'
-        ]
-    ]
-];
-$productoSeleccionado = isset($_POST['producto']) ? $_POST['producto'] : 'todos';
-
-*/
-
 $productoSeleccionado = $_GET['producto'] ?? null;
 
 require_once('consultas/conexion.php');
 require_once('consultas/consultas_productos.php');
 
 $productos = getProductos($conexion, $productoSeleccionado);
-
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -114,7 +24,6 @@ $productos = getProductos($conexion, $productoSeleccionado);
             </ul>
         </nav>
     </header>
-
     <main>
         <h1>Bienvenido a nuestro catálogo</h1>
         <p><strong>Aquí encontrarás nuestros productos para que puedas pedir en la caja</strong></p>
@@ -130,45 +39,12 @@ $productos = getProductos($conexion, $productoSeleccionado);
             <button type="submit">Filtrar</button>
         </form>
         <section class="fondoMenu">
-
             <?php foreach($productos as $producto): ?>
                 <article>
                     <img src="<?php echo $producto['imagen']; ?>" alt="<?php echo $producto['nombre']; ?>">
                     <h3><?php echo $producto['nombre']; ?></h3>
                 </article>
             <?php endforeach ?>
-
-            <?php 
-            /* COMENTADO POR EL PROFE */
-            /*if ($productoSeleccionado == 'todos' || $productoSeleccionado == 'cafe') : ?>
-                <h2>Café</h2>
-                <?php foreach ($productos['cafe'] as $producto) : ?>
-                    <article>
-                        <img src="<?php echo $producto['imagen']; ?>" alt="<?php echo $producto['nombre']; ?>">
-                        <h3><?php echo $producto['nombre']; ?></h3>
-                    </article>
-                <?php endforeach; ?>
-            <?php endif; ?>
-
-            <?php if ($productoSeleccionado == 'todos' || $productoSeleccionado == 'comida') : ?>
-                <h2>Comidas</h2>
-                <?php foreach ($productos['comida'] as $producto) : ?>
-                    <article>
-                        <img src="<?php echo $producto['imagen']; ?>" alt="<?php echo $producto['nombre']; ?>">
-                        <h3><?php echo $producto['nombre']; ?></h3>
-                    </article>
-                <?php endforeach; ?>
-            <?php endif; ?>
-
-            <?php if ($productoSeleccionado == 'todos' || $productoSeleccionado == 'bebidas') : ?>
-                <h2>Bebidas</h2>
-                <?php foreach ($productos['bebidas'] as $producto) : ?>
-                    <article>
-                        <img src="<?php echo $producto['imagen']; ?>" alt="<?php echo $producto['nombre']; ?>">
-                        <h3><?php echo $producto['nombre']; ?></h3>
-                    </article>
-                <?php endforeach; ?>
-            <?php endif; */ ?>
         </section>
     </main> 
     <footer>
