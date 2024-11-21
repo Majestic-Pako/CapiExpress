82fe7b6b6a988499f10c0790e9fe59872c0b377f
Majestic-Pako <agustinchoque484@gmail.com>
2024-11-21 13:02:22 -0300

    problema resuelto)?
    Al parecer pude arreglar el problema de la barra blanca

--- Index.php
+++ Index.php
@@ -20,7 +20,40 @@
         </nav>
     </header>
     <main>
-        
+        <h1>ACA ESTARIA EL INICIO</h1>
     </main>
+    <footer class="footer">
+        <ul class="social-icon">
+            <li class="icon-elem">
+                <a href="https://github.com/Majestic-Pako/CapiExpress.git" class="icon">
+                <ion-icon name="logo-github"></ion-icon>
+                </a>
+            </li>
+            <li class="icon-elem">
+                <a href="https://www.instagram.com/escueladavinci/?hl=es" class="icon">
+                <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+            </li>
+            <li class="icon-elem">
+                <a href="https://youtu.be/BcGAPkjt_IE?si=ziQ-VTYzYdT3wx_R" class="icon">
+                <ion-icon name="logo-youtube"></ion-icon>
+                </a>
+            </li>
+        </ul>
+        <ul class="fin">
+            <li class="fin-elem">
+                <a href="index.php" class="fin-icon">Inicio</a>
+            </li>
+            <li class="fin-elem">
+                <a href="Menu.php" class="fin-icon">Menu</a>
+            </li>
+            <li class="fin-elem">
+                <a href="#" class="fin-icon">Sobre nosotros</a>
+            </li>
+        </ul>
+        <p class="text">&copy;CapiExpress | Todos los derechos reservados </p>
+    </footer>
+    <script  type = "module"  src = " https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js " > </script> 
+    <script nomodule  src  = " https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js " > </script>
 </body>
 </html>
