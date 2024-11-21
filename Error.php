f8da548eaf6f0cf4ece68178d53bfc81ce12ac21
Anthony2080 <antonio.pirotta@davinci.edu.ar>
2024-11-21 15:28:57 -0300

    Pagina de Error
    Ya declarado la pagina de error

---
+++ Error.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>ERROR</title>
+</head>
+<body>
+<header>
+    <nav>
+            <div class = "navi">
+                <img src="img/Capi.png" alt="Capibara">
+            </div>
+            <ul>
+                <li><a href="Index.php">Inicio</a></li>
+                <li><a href="Menu.php">Menú</a></li>
+                <li><a href="Historia.php">Sobre nosotros</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="error-text">
+        <h1>Ups!</h1>
+        <h2>Tenemos problemas tecnicos<h2>
+            <p>Vuelva a intentar mas tarde<p>
+        <img src="img/error.png" alt="capibara llorando" height="400" width="400">
+    </main>
+    <footer class="footer">
+        <ul class="social-icon">
+            <li class="icon-elem">
+                <a href="https://github.com/Majestic-Pako/CapiExpress.git" class="icon">
+                <ion-icon name="logo-github"></ion-icon>
+                </a>
+            </li>
+            <li class="icon-elem">
+                <a href="https://www.instagram.com/escueladavinci/?hl=es" class="icon">
+                <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+            </li>
+            <li class="icon-elem">
+                <a href="https://youtu.be/BcGAPkjt_IE?si=ziQ-VTYzYdT3wx_R" class="icon">
+                <ion-icon name="logo-youtube"></ion-icon>
+                </a>
+            </li>
+        </ul>
+        <ul class="fin">
+            <li class="fin-elem">
+                <a href="index.php" class="fin-icon">Inicio</a>
+            </li>
+            <li class="fin-elem">
+                <a href="Menu.php" class="fin-icon">Menu</a>
+            </li>
+            <li class="fin-elem">
+                <a href="#" class="fin-icon">Sobre nosotros</a>
+            </li>
+        </ul>
+        <p class="text">&copy;CapiExpress | Todos los derechos reservados </p>
+    </footer>
+    <script  type = "module"  src = " https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js " > </script> 
+    <script nomodule  src  = " https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js " > </script>
+</body>
+</html>
