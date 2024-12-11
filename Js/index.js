cdccda76e7e9063b28ffe10d568aee0f94a08635
Majestic-Pako <agustinchoque484@gmail.com>
2024-12-11 11:38:47 -0300

    Correcciones
    Corregido el error de que no se mostraba la pagian del registro

--- Js/index.js
+++ Js/index.js
@@ -20,15 +20,15 @@ inputs.forEach(input => {
 var linkTextLogin = document.getElementById("linkTextLogin");
 var linkTextRegistro = document.getElementById("linkTextRegistro");
 var formLogin = document.querySelector(".contenedor-form-login");
-var formRegistro = document.querySelector(".contenedor-form-registro");
+var formRegistro = document.querySelector(".contenerdor-form-registro");
 
-linkTextLogin.addEventListener("click",function(){
+linkTextLogin.addEventListener("click",function(event){
     event.preventDefault();
     formLogin.classList.add('inactive');
     formRegistro.classList.remove('inactive');
 })
 
-linkTextRegistro.addEventListener("click",function(){
+linkTextRegistro.addEventListener("click",function(event){
     event.preventDefault();
     formLogin.classList.remove('inactive');
     formRegistro.classList.add('inactive');
