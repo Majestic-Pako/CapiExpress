3bc15d537b48a1abeaa263b8a0f55be9b28ee160
Majestic-Pako <agustinchoque484@gmail.com>
2024-12-13 14:10:00 -0300

    Index Gran Update
    Ya hice el diseño debajo del banner del inicio con unos beneficios y una galería con los horarios, no se espera mas actualizaciones para agregar cosas

--- Index.php
+++ Index.php
@@ -19,33 +19,46 @@
                 <a href="Menu.php">Mira Nuestro Menu!!!</a>
             </div>
         </section>
-          <!-- Sección para las tarjetas -->
-         <section class="cards-container">
-        <!-- Card 1 -->
-        <div class="card">
-            <img src="img/cafe100.png" alt="Café 1" class="card-img">
-            <h3 class="card-title">Café Clásico</h3>
-            <p>Disfruta de nuestro café clásico, 100% arábica, con un sabor suave y equilibrado.</p>
+        <div class="subtitulo-index">
+            <h2>Algunos de Nuestros beneficios</h2>
         </div>
-        <!-- Card 2 -->
-        <div class="card">
-            <img src="img/cafeleche.png" alt="Café 2" class="card-img">
-            <h3 class="card-title">Café con Leche</h3>
-            <p>Un delicioso café con leche que te hará sentirte en casa.</p>
-        </div>
-        <!-- Card 3 -->
-        <div class="card">
-            <img src="img/expresso.png" alt="Café 3" class="card-img">
-            <h3 class="card-title">Café Expresso</h3>
-            <p>La intensidad del café expreso con una capa de crema, ideal para los amantes del café fuerte.</p>
-        </div>
-    </section>
+        <section class="contenedor-cardu">
+            <div class="cardu">
+                <ion-icon name="star-half-outline" class="icono-cardu"></ion-icon>
+                <h3>Servicio Excelente</h3>
+            </div>
+            <div class="cardu">
+            <ion-icon name="wifi-outline" class="icono-cardu"></ion-icon>
+                <h3>Wifi Gratis</h3>
+            </div>
+            <div class="cardu">
+            <ion-icon name="paw-outline" class="icono-cardu"></ion-icon>
+                <h3>Permitimos Mascotas</h3>
+            </div>
+        </section>
+        <section class="galeria">
+				<img
+					src="img/Galeria-1.png"
+					class="galeria-img-1" 
+				/><img
+					src="img/Galeria-2.png"
+					class="galeria-img-2"
+				/><img
+					src="img/Horarios.png" 
+					class="galeria-img-3"
+				/><img
+					src="img/Galeria-3.png"
+					class="galeria-img-4"
+				/><img
+					src="img/Galeria-4.png"
+					class="galeria-img-5"
+				/>
+			</section>
     </main>
     <?php
         require_once('./layout/footer.php');
     ?>
     <script  type = "module"  src = " https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js " > </script> 
     <script nomodule  src  = " https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js " > </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
